5e4e0065a33ac85b2ac12d81f728f9318067f9fa
AdamQureshiGH <122238224+AdamQureshiGH@users.noreply.github.com>
2025-03-12 20:40:40 -0400

    Initial commit

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello and welcome to my day 1  of python band name generator!")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your band name could be: " + city + " " + pet_name + "\n")
