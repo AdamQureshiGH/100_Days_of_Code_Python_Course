96769dfcfb737ea434b2d0b4728db09522c04e23
AdamQureshiGH <122238224+AdamQureshiGH@users.noreply.github.com>
2025-05-04 16:57:56 -0400

    Day 8 & 9 coming back from hiatus

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 1/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 1/main.py
@@ -1,18 +1,13 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+print(art.logo)
+game_over = True
 
 
 def caesar(original_text, shift_amount, direction):
+
     cipher_text = ""
     if (direction == "encode"):
         shift_amount * 1
@@ -22,10 +17,25 @@ def caesar(original_text, shift_amount, direction):
         print("Choose encode or decode")
 
     for letter in original_text:
+        if(not letter.isalpha()):
+            cipher_text += letter
+            continue
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
     print(f"Text: {cipher_text}")
 
-caesar(text, shift, direction)
+while(game_over == True):
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt, type 'stop' to terminate the program:\n").lower()
+    if (direction == "stop"):
+        game_over = False
+        break
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if(not shift.is_integer()):
+        print("Invalid Input")
+        break
+    caesar(text, shift, direction)
+
 
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Blind Auction Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Blind Auction Project/main.py
@@ -2,5 +2,26 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+import art
+other_bidders = ""
+print("\n" * 100)
+print(art.logo)
+dict = {}
+def auction(dict):
+    highest_bidder = ""
+    highest_value = 0
+    while (True):
+        if (input("Are there any other bidders? (yes/no)\n").lower() == "no"):
+            break
+        Key = input("Name:\n")
+        Value = input("Amount to bid:\n")
+        dict[Key] = Value
 
+    for key in dict:
+        if (float(dict[key]) > highest_value):
+            highest_value = float(dict[key])
+            highest_bidder = key
 
+    print(f"The winner is {highest_bidder}, with a bid of ${highest_value}")
+
+auction(dict)
